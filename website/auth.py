f697b78d1352029a9e8fe3102ee3caa5c934c1f1
Mateusz Majkusiak <mateusz.majkusiak.ltsf@gmail.com>
2023-11-01 12:25:45 +0100

    POST ang GET requests handling

--- website/auth.py
+++ website/auth.py
@@ -1,8 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
+# setting endpoints
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    data = request.form
+    print(data)
     return render_template("login.html", boolean=True)
 
 @auth.route('/logout')
@@ -11,4 +14,21 @@ def logout():
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_upt():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        # example tests for form credentials
+        if len(email) < 4:
+            pass
+        elif len(firstName) < 2:
+            pass
+        elif password1 != password2:
+            pass
+        elif len(password1) < 7:
+            pass
+        else:
+            # add user to the DB
+            pass
     return render_template("sign_up.html")
