9705f6c9e69453a7a54b74058267b177c891ca70
Mateusz Majkusiak <mateusz.majkusiak.ltsf@gmail.com>
2023-11-01 17:57:21 +0100

    check if login function is working and can't sign up using existing email

--- website/auth.py
+++ website/auth.py
@@ -9,8 +9,20 @@ auth = Blueprint('auth', __name__)
 # setting endpoints
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    data = request.form
-    print(data)
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in succesfully!', category='success')
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again', category='error')
+        else:
+            flash('email does not exist.', category='error')
+
     return render_template("login.html", boolean=True)
 
 @auth.route('/logout')
@@ -24,14 +36,18 @@ def sign_up():
         first_name = request.form.get('first_name')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists.', category='error')
         # example tests for form credentials - with flashing messages
-        if len(email) < 4:
+        elif len(email) < 4:  # type: ignore
             flash('Email must be greater than 3 characters.', category='error')
-        elif len(first_name) < 2:
+        elif len(first_name) < 2:  # type: ignore
             flash('First name must be greater than 1 character.', category='error')
         elif password1 != password2:
             flash('Passwords don\'t match.', category='error')
-        elif len(password1) < 9:
+        elif len(password1) < 9:  # type: ignore
             flash('Password must be at least 8 characters.', category='error')
         else:
             new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1))
