1f3a63c6c3d98c2703c77fc21c74f51611d991d3
Mateusz Majkusiak <mateusz.majkusiak.ltsf@gmail.com>
2023-11-01 12:35:21 +0100

    handling flashing messages for credentials test

--- website/auth.py
+++ website/auth.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
+
 # setting endpoints
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -13,22 +14,23 @@ def logout():
     return "<p>Logout</p>"
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
-def sign_upt():
+def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
+        first_name = request.form.get('first_name')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
-        # example tests for form credentials
+        # example tests for form credentials - with flashing messages
         if len(email) < 4:
-            pass
-        elif len(firstName) < 2:
-            pass
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(first_name) < 2:
+            flash('First name must be greater than 1 character.', category='error')
         elif password1 != password2:
-            pass
-        elif len(password1) < 7:
-            pass
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 9:
+            flash('Password must be at least 8 characters.', category='error')
         else:
             # add user to the DB
-            pass
+            flash('Account created!', category='success')
+
     return render_template("sign_up.html")
