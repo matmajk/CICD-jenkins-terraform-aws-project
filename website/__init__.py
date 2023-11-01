b8548bae162bc273fd950bdb7cd77484e962689c
Mateusz Majkusiak <mateusz.majkusiak.ltsf@gmail.com>
2023-11-01 16:21:07 +0100

    DB creation

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
+
 
 def create_app():
     app = Flask(__name__)
@@ -15,6 +17,11 @@ def create_app():
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+
+    from .models import User, Note
+
+    with app.app_context():
+        db.create_all()
 
     return app
 
