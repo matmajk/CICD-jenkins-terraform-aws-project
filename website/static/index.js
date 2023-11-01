48c7188e9e1b9c59591ac370828312f567973497
Mateusz Majkusiak <mateusz.majkusiak.ltsf@gmail.com>
2023-11-02 00:32:18 +0100

    fix .js function

--- website/static/index.js
+++ website/static/index.js
@@ -1,8 +1,8 @@
 function deleteNote(noteId) {
-    fetch('/delete-note'), {
+    fetch("/delete-note", {
         method: "POST",
         body: JSON.stringify({ noteId: noteId }),
-    }).then((_res)) => {
+    }).then((_res) => {
         window.location.href = "/";
     });
 }
