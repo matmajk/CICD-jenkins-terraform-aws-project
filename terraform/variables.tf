6a5f1387e0631335ef51f89b4ed6f984863a2a53
Mateusz Majkusiak <mateusz.majkusiak.ltsf@gmail.com>
2023-11-06 01:52:43 +0100

    update providers.tf and variables.tf files

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,33 +1,100 @@
 variable "aws_region" {
-  type = string
+  type        = string
   description = "Region used for AWS resources"
-  default = "eu-central-1"
+  default     = "eu-central-1"
 }
 
 variable "availability_zone" {
-  type = "string"
+  type    = string
   default = "eu-central-1a"
 }
 
 variable "user" {
-  type = "string"
+  type    = string
   default = "ubuntu"
 }
 
 variable "instance_type" {
-  type = "string"
+  type        = string
   description = "EC2 Instance type"
-  default = "t2.micro"
+  default     = "t2.micro"
 }
 
-variable "aws_access_key" {
-  type = "string"
-  description = "Access key for AWS"
-  sensitive = "true"
+variable "instance_name" {
+  type = string
+  description = "EC2 Instance name"
+  default = "terraform-instance"
 }
 
-variable "aws_secret_key" {
-  type = "string"
-  descriptions = "Secret key for AWS"
-  sensitive = "true"
+#variable "aws_access_key" {
+#  type        = string
+#  description = "Access key for AWS"
+#  sensitive   = "true"
+#}
+#
+#variable "aws_secret_key" {
+#  type        = string
+#  description = "Secret key for AWS"
+#  sensitive   = "true"
+#}
+
+variable "key_name" {
+  type        = string
+  description = ".ppk / .pem Key name "
+  default     = "terraform-key"
+}
+
+variable "public_key" {
+  type    = string
+  default = "terraform-key.pub"
+}
+
+variable "private_key" {
+  type    = string
+  default = "terraform-key"
+}
+
+variable "default_cidr_route" {
+  type        = string
+  description = "Default route CIDR"
+  default     = "0.0.0.0/0"
+}
+
+variable "vpc_cidr_block" {
+  type        = string
+  description = "Base CIDR block for VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "vpc_subnet_cidr_block" {
+  type        = string
+  description = "CIDR block for subnet in VPC"
+  default     = "10.0.1.0/24"
+}
+
+variable "map_public_ip_on_launch" {
+  type        = bool
+  description = "Map a public IP for Subnet instance"
+  default     = true
+}
+
+variable "enable_dns_hostnames" {
+  type        = bool
+  description = "Enable DNS hostnames in VPC"
+  default     = true
+}
+
+variable "project" {
+  type        = string
+  description = "Project name for resource tagging"
+}
+
+variable "company" {
+  type        = string
+  description = "Company name for resource tagging"
+}
+
+variable "category" {
+  type        = string
+  description = "Category for resource tagging"
 }
