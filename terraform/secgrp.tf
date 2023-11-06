d23d6247c7f5831eb92f4b62d459788eea6d967e
Mateusz Majkusiak <mateusz.majkusiak.ltsf@gmail.com>
2023-11-06 01:54:19 +0100

    secgrp.tf file

---
+++ terraform/secgrp.tf
@@ -0,0 +1,31 @@
+resource "aws_security_group" "terraform-sg" {
+  vpc_id      = aws_vpc.terraform-vpc.id
+  name        = "terraform-sg"
+  description = "Security group for MyIP SSH and for traffic from anywhere"
+
+  ingress {
+    description = "SSH connection from MyIP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.default_cidr_route]
+  }
+
+  ingress {
+    description = "HTTP access from anywhere"
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = [var.default_cidr_route]
+  }
+
+  egress {
+    description = "Outbound internet access"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [var.default_cidr_route]
+  }
+
+  tags = local.resource_tags
+}
