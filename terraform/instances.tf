1bfa7b785d8bbdeb8867923f73c3541964641dde
Mateusz Majkusiak <94702824+matmajk@users.noreply.github.com>
2024-01-10 22:14:15 +0100

    test update instances.tf

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -1,11 +1,15 @@
-resource "aws_key_pair" "terraform-key" {
-  key_name   = var.key_name
-  public_key = tls_private_key.rsa.public_key_openssh
-}
-
 resource "tls_private_key" "rsa" {
   algorithm = "RSA"
   rsa_bits = 4096
+}
+
+resource "aws_key_pair" "terraform-key" {
+  key_name   = var.key_name
+  public_key = tls_private_key.rsa.public_key_openssh
+
+  provisioner = "local-exec" {
+    command = "echo '${tls_private_key.rsa.private_key_pem}' > ./terraform-key.pem"
+    }
 }
 
 resource "local_file" "terraform-key" {
