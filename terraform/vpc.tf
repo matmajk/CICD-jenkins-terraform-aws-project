8650751112ca848dd6de6ab578bb37dbfc23804b
Mateusz Majkusiak <mateusz.majkusiak.ltsf@gmail.com>
2024-01-14 01:27:39 +0100

    network vpc updates

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,13 +1,13 @@
-resource "aws_vpc" "terraform-vpc" {
-  cidr_block           = var.vpc_cidr_block
-  instance_tenancy     = "default"
-  enable_dns_hostnames = var.enable_dns_hostnames
-
-  tags = local.resource_tags
-}
+#  resource "aws_vpc" "terraform-vpc" {
+#    cidr_block           = var.vpc_cidr_block
+#    instance_tenancy     = "default"
+#    enable_dns_hostnames = var.enable_dns_hostnames
+#  
+#    tags = local.resource_tags
+#  }
 
 resource "aws_subnet" "terraform-subnet-public" {
-  vpc_id                  = aws_vpc.terraform-vpc.id
+  vpc_id                  = var.vpc_id
   cidr_block              = var.vpc_subnet_cidr_block
   map_public_ip_on_launch = var.map_public_ip_on_launch
   availability_zone       = var.availability_zone
@@ -15,24 +15,24 @@ resource "aws_subnet" "terraform-subnet-public" {
   tags = local.resource_tags
 }
 
-resource "aws_internet_gateway" "terraform-igw" {
-  vpc_id = aws_vpc.terraform-vpc.id
+#  resource "aws_internet_gateway" "terraform-igw" {
+#    vpc_id = var.vpc_id
+#  
+#    tags = local.resource_tags
+#  }
 
-  tags = local.resource_tags
-}
-
-resource "aws_route_table" "terraform-rtb" {
-  vpc_id = aws_vpc.terraform-vpc.id
-
-  route {
-    cidr_block = var.default_cidr_route
-    gateway_id = aws_internet_gateway.terraform-igw.id
-  }
-
-  tags = local.resource_tags
-}
-
-resource "aws_route_table_association" "terraform-subnet-rta" {
-  subnet_id      = aws_subnet.terraform-subnet-public.id
-  route_table_id = aws_route_table.terraform-rtb.id
-}
+#  resource "aws_route_table" "terraform-rtb" {
+#    vpc_id = var.vpc_id
+#  
+#    route {
+#      cidr_block = var.default_cidr_route
+#      gateway_id = aws_internet_gateway.terraform-igw.id
+#    }
+#  
+#    tags = local.resource_tags
+#  }
+#  
+#  resource "aws_route_table_association" "terraform-subnet-rta" {
+#    subnet_id      = aws_subnet.terraform-subnet-public.id
+#    route_table_id = aws_route_table.terraform-rtb.id
+#  }
