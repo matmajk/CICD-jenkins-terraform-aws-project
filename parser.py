e7a19303bd71ae0e9d2dcedb47387e88918bdfdc
Mateusz Majkusiak <mateusz.majkusiak.ltsf@gmail.com>
2024-01-16 02:24:54 +0100

    restore chenges

--- parser.py
+++ parser.py
@@ -9,7 +9,7 @@ terraform_instance = []
 for dns in aws_public_instance_dns:
     terraform_instance.append("ansible_host=" + dns)
 
-with open("/var/lib/jenkins/workspace/hosts", "w") as f:
+with open("ansible/hosts", "w") as f:
     f.writelines("# hosts\n\n")
     f.writelines("[instances]\n")
     for i in range(len(terraform_instance)):
